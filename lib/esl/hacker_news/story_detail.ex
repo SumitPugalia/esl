febb6503a36369a75a01d75beba10beac730cd03
SumitPugalia <sumit_pugalia@yahoo.com>
2020-12-16 20:58:58 +0400

    add comments to explain code

--- lib/esl/hacker_news/story_detail.ex
+++ lib/esl/hacker_news/story_detail.ex
@@ -1,5 +1,5 @@
 defmodule Esl.HackerNews.StoryDetail do
-    
+    ## FETCH THE DETAIL OF STORY FOR THE GIVEN ID FROM HACKER NEWS
     @spec fetch_item(number) :: {:error, String.t | Jason.DecodeError.t()} | {:ok, any}
     def fetch_item(id) do
       url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json?print=pretty"
@@ -8,6 +8,6 @@ defmodule Esl.HackerNews.StoryDetail do
         200 -> Jason.decode(response.body)
         http_code -> {:error, "Bad response with http code: #{http_code}"}
       end
-      
+
     end
   end
--- lib/esl/top_stories/worker.ex
+++ lib/esl/top_stories/worker.ex
@@ -1,14 +1,14 @@
 defmodule Esl.TopStories.Worker do
     use GenServer
     @moduledoc false
-    
+
     alias Esl.HackerNews
-    
+
     @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
     def start_link(opts) do
       GenServer.start_link(__MODULE__, opts, name: __MODULE__)
     end
-    
+
     @spec retrieve_data(number, number, number) :: {:ok, number, list(map)} | {:error, String.t}
     def retrieve_data(id, page_number, page_size) do
         GenServer.call(__MODULE__, {:retrieve, id, page_number, page_size})
@@ -16,8 +16,9 @@ defmodule Esl.TopStories.Worker do
 
     @spec retrieve_detail(number) :: {:error, String.t} | {:ok, map}
     def retrieve_detail(id) do
+        ## running the logic to find the match in client sothat the server isnot block performing the find operation
         %{versions: [latest, old], news: news} = GenServer.call(__MODULE__, :retrieve_state)
-        
+
         Map.get(news, latest)
         |> Enum.concat(Map.get(news, old))
         |> Enum.find(fn item -> Map.get(item, "id")  == id end)
@@ -34,44 +35,44 @@ defmodule Esl.TopStories.Worker do
             {:error, any} | {:ok, %{news: %{optional(integer) => []}, versions: [integer, ...]}}
     def init(_) do
       poll_period = Application.get_env(:esl, :poll_period)[:hacker_news]
-      
+
       # initial message to fetch_data
       Process.send(self(), :fetch_data, [])
-      
+
       # sending the fetch_data message every poll_period seconds
       case :timer.send_interval(poll_period * 1_000, :fetch_data) do
         {:ok, _tref} ->
 
             old = System.system_time(:millisecond)
-            #  to have different values for versions 
+            #  to have different values for versions
             latest = System.system_time(:millisecond) + 1
-            
+
             news = %{old => [], latest => []}
             {:ok, %{versions: [latest, old], news: news}}
-        
-        {:error, error} -> 
+
+        {:error, error} ->
             IO.inspect(error)
             {:error, error}
       end
     end
-    
+
     @impl true
     def handle_call({:retrieve, id, page_number, page_size}, _from, %{versions: [latest, _old], news: news} = state) do
         id = if is_nil(id), do: latest, else: id
 
-        response = 
+        response =
             case Map.get(news, id) do
                 nil -> {:error, "version is too old to fetch data, try the latest version"}
                 data ->
-                    top_news = 
-                        data 
-                        |> Enum.chunk_every(page_size) 
+                    top_news =
+                        data
+                        |> Enum.chunk_every(page_size)
                         |> Enum.at(page_number)
-                    
+
                     case top_news do
                         nil -> {:error, "no data found for this page number"}
-                        top_news -> {:ok, id, top_news} 
-                    end 
+                        top_news -> {:ok, id, top_news}
+                    end
             end
 
         {:reply, response, state}
@@ -83,7 +84,7 @@ defmodule Esl.TopStories.Worker do
 
     @impl true
     def handle_info(:fetch_data, state) do
-        spawn(fn -> 
+        spawn(fn ->
             case HackerNews.Worker.fetch() do
                 {:ok, data} ->
                     Process.send(__MODULE__, {:update_data, data}, [])
